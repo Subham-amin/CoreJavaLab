4b2f69702703d2b403dc450298ed2055502ac440
SUBHAM AMIN <113018980+Subham-amin@users.noreply.github.com>
2022-09-15 13:09:32 +0530

    Update Quotient.java

--- 08-Sep-2022/Quotient.java
+++ 08-Sep-2022/Quotient.java
@@ -1,7 +1,5 @@
 /*
-Program: Add 2 numbers
-@author: Shuruti Singhal
-@Date: 6 sep 2022
+Program for Division with remainder output
 */
 
 //decalraing a class
@@ -14,7 +12,7 @@ class Quotient
 	  int num1= Integer.parseInt(args[0]); //taking input1
 	  int num2= Integer.parseInt(args[1]); //taking input2 
 	 System.out.println("Your Division is");
-	 System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));//Printing Diision output. 
+	 System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));//Printing Division output. 
 	 System.out.println("Your Remeinder is");
 	 System.out.println(Integer.parseInt(args[0])%Integer.parseInt(args[1]));//Printing remeinedr output.
 	 
@@ -24,4 +22,4 @@ class Quotient
  // end of main
 
 }
-// end of class AdditionDemo
+// end of class
